57b0cd5b24f85f2942311e74301afb13e6344603
Vadym <tecey1@gmail.com>
2017-08-01 19:30:51 +0300

    Test 2

---
+++ Lab1/src/com/FirstStart/Main.java
@@ -0,0 +1,15 @@
+package com.FirstStart;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();
+        Scanner sd = new Scanner(System.in);
+        int line2 = sd.nextInt();
+        System.out.println(line + line2);
+    }
+}
