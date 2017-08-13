2f7ded3b80dfb0f67a12f3bc37c284a524fb0843
Vadym <tecey1@gmail.com>
2017-08-13 23:13:37 +0300

    init

--- Lab1/src/com/FirstStart/Lab43.java
+++ Lab1/src/com/FirstStart/Lab43.java
@@ -8,6 +8,21 @@ XOR) for two variables of boolean data type.
 public class Lab43 {
     public static void main(String args[]) {
 
+        boolean a = true;
+        boolean b = false;
+
+        boolean c = a & b;      // AND
+        System.out.println(c);
+
+        boolean v = a | b;      // OR
+        System.out.println(v);
+
+        boolean j = a != b;     // NOT
+        System.out.println(j);
+
+        boolean r = a ^ b;      // XOR
+        System.out.println(r);
+
 
     }
 }
