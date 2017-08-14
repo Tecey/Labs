f8409afda7611ad019ca1bd6e3325a96d7cde676
Vadym <tecey1@gmail.com>
2017-08-14 23:24:54 +0300

    init

--- Lab1/src/com/FirstStart/Lab45.java
+++ Lab1/src/com/FirstStart/Lab45.java
@@ -1,8 +1,27 @@
 package com.FirstStart;
 
+/*
+Why not 5 ?
+ */
+
 public class Lab45 {
     public static void main(String args[]) {
 
-        System.out.println(a);
+        int a = 10;
+
+        int b = a << 1;
+        System.out.println(b);
+
+        int b2 = (a/2)*2 << 1;  // Why not 5 ?
+        System.out.println(b2);
+
+        int c = a >> 1;
+        System.out.println(c);
+
+        int d = a >>> 1;
+        System.out.println(d);
+
+        int w = a >> -1;
+        System.out.println(w);
     }
 }
---
+++ Lab1/src/com/FirstStart/Lab48.java
@@ -0,0 +1,12 @@
+package com.FirstStart;
+
+/*
+Write a console program that prints result type casting for different primitive types.
+ */
+
+public class Lab48 {
+    public static void main(String args[]) {
+
+        System.out.println(z);
+    }
+}
