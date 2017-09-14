79cb2bbdbd4a4917b284d149aa0d0d2b875d1729
Vadym <tecey1@gmail.com>
2017-09-14 19:11:01 +0300

    init

--- Lab1/src/com/FirstStart/Lab54.java
+++ Lab1/src/com/FirstStart/Lab54.java
@@ -4,23 +4,30 @@ import java.util.Scanner;
 /*
 Writing simple console application, using nested-loop and if-else construction.
 
+Программа должна печатать первые 10 чисел, которые делятся равномерно на 3 или 4, но для
+цикл выполняет итерацию от 1 до 300 (поэтому оператор break должен использоваться после 10 результатов печати).
+Результат должен выглядеть так:
+3
+4
+...
+20
  */
 
-public class Lab54 {
-    public static void main(String[] args) {
-        System.out.print("Введите число 1, 2 или 3: ");
-        Scanner inputFigure = new Scanner(System.in);
-        int i = inputFigure.nextInt();
-
-        if (i == 1) {
-            System.out.println("Вы ввели число 1");
-        } else if (i == 2) {
-            System.out.println("Вы ввели число 2");
-        } else if (i == 3) {
-            System.out.println("Вы ввели число 3");
-        } else {
-            System.out.println("Вы ввели число не равное 1, 2 или 3");
-
-        }
-    }
-}
+//public class Lab54 {
+//    public static void main(String[] args) {
+//        System.out.print("Введите число 1, 2 или 3: ");
+//        Scanner inputFigure = new Scanner(System.in);
+//        int i = inputFigure.nextInt();
+//
+//        if (i == 1) {
+//            System.out.println("Вы ввели число 1");
+//        } else if (i == 2) {
+//            System.out.println("Вы ввели число 2");
+//        } else if (i == 3) {
+//            System.out.println("Вы ввели число 3");
+//        } else {
+//            System.out.println("Вы ввели число не равное 1, 2 или 3");
+//
+//        }
+//    }
+//}
---
+++ Lab1/src/com/FirstStart/Lab57.java
@@ -0,0 +1,26 @@
+package com.FirstStart;
+
+/*
+Remind that student should apply his or her knowledge of:
+• how to use nested-loop, if-else construction and arithmetic operators
+Execute following steps:
+• Student should create a new blank Java Project in IDE.
+• Student should create his or her own Java class and provide all necessary code in
+a method main()
+• Student should enter the n using the Scanner
+• Student should use nested-loop, if-else construction and arithmetic operators.
+Ask student to execute the program
+
+Напомним, что студент должен применить свои знания:
+• как использовать вложенные, if-else конструкции и арифметические операторы
+Выполните следующие шаги:
+• Студент должен создать новый пустой Java-проект в среде IDE.
+• Студент должен создать свой собственный класс Java и предоставить весь необходимый код в
+метод main ()
+• Студент должен ввести n, используя сканер
+• Студент должен использовать вложенные, if-else конструкции и арифметические операторы.
+Попросите учащегося выполнить программу
+ */
+
+public class Lab57 {
+}
