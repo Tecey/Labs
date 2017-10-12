31b9470cfc892555067aece45ee23cb4bdec2573
Vadym <tecey1@gmail.com>
2017-10-12 19:19:11 +0300

    init

---
+++ LabWork/src/Lab1_5/Lab54_help.java
@@ -0,0 +1,41 @@
+package src.Lab1_5;
+import java.util.Scanner;
+
+/*
+Writing simple console application, using nested-loop and if-else construction.
+
+Программа должна печатать первые 10 чисел, которые делятся равномерно на 3 или 4, но для
+цикл выполняет итерацию от 1 до 300 (поэтому оператор break должен использоваться после 10 результатов печати).
+Результат должен выглядеть так:
+3
+4
+...
+20
+ */
+
+public class Lab54_help {
+    public static void main(String[] args) {
+
+//        int i, j, a = 1;
+//
+//        for (i = 3; i < 31; i + 3) {
+//            System.out.println(i);
+//
+//
+//            for (j = 4; j < 41; j + 4) {
+//                System.out.println(j);
+//            }
+//        }
+
+//        if (a <== 10) {
+//            System.out.println("1. " + a);
+//            a++;
+//        }
+//        else if (a == 11) {
+//            System.out.println("Первые 10 чисел от единицы, что деляьтся на 3 и 4, выведены на экран.");
+//        }
+
+
+    }
+
+}
---
+++ PracticeCodingBase/src/FirstStart/Computer.java
@@ -0,0 +1,22 @@
+package src.FirstStart;
+
+
+public class Computer {
+    private String manufacturer;
+    private int serialNomber;
+        public float price = 2.4f; // потом вернуть и в маин сделать через сет и гет
+    private int quantityCPU;
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+    // создать для всех остальных 2,1.3
+
+    public void view() {
+        System.out.println(manufacturer + ", " + serialNomber + ", " + price + ", " + quantityCPU);
+    }
+}
